b311e7ade8bff223f62c8f123c02c5d06ac270ec
bodnar22 <kovrikpodmyshu@gmail.com>
2023-06-19 20:47:46 +0300

    initial commit

---
+++ nomenc/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NomencConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nomenc'
---
+++ nomenc/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, db_index=True, verbose_name="Имя")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(unique=True, verbose_name="Имя")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    status = models.CharField(choices=[('in_stock', 'In stock'), ('out_of_stock', 'Out of stock')]
+                              , verbose_name="Статус")
+    remains = models.IntegerField(verbose_name="Остаток")
+
+    def __str__(self):
+        return self.name
